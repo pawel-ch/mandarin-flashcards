7b4804b1b571c8e9d741695117d919f8574ed98d
Paweł Chojnowski <pawel.chojnowski@live.com>
2021-03-08 02:36:46 +0100

    Black + refactor

--- createWordFlashcards.py
+++ createWordFlashcards.py
@@ -16,111 +16,131 @@
     The formatting is meant for printing on pre-perforating business card paper. 
 """
 import codecs
-import pinyin
 import datetime
 import re
+from itertools import chain, islice
+
+import pinyin
 from docx import Document
-from docx.shared import Pt, Inches
 from docx.enum.style import WD_STYLE_TYPE
-from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.enum.table import WD_ROW_HEIGHT_RULE, WD_ALIGN_VERTICAL
+from docx.enum.table import WD_CELL_VERTICAL_ALIGNMENT, WD_ROW_HEIGHT_RULE
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.shared import Inches, Pt
 
-vocab = []
-with codecs.open("flashcard-input.txt", "r", "utf-8-sig") as file:
-    for line in file.read().splitlines():
-        entry = line.split(None, 1)
-        vocab.append(entry)
+CHAR_COUNT_TO_STYLE = {
+    1: "Hanzi Style 115",
+    2: "Hanzi Style 100",
+    3: "Hanzi Style 80",
+    4: "Hanzi Style 60",
+    5: "Hanzi Style 45",
+}
 
-pinyin_list = []
-for zi_with_example in vocab:
-    zi = zi_with_example[0]
-    example = ""
-    if len(zi_with_example) > 1:
-        zi_info = zi_with_example[1]
-        if zi_info.startswith('('): 
-            english = zi_info[zi_info.find("("):zi_info.find(")")+1]
-            example = zi_info[zi_info.find(")")+1:]
-        else:
-            english = ""
-            example = zi_info
-        pinyin_list.append([pinyin.get(zi) + " ",english,example])
 
-doc = Document('FlashcardTemplate.docx')
-doc_table = doc.tables[0]
+def chunks(iterable, size=10):
+    iterator = iter(iterable)
+    for first in iterator:
+        yield chain([first], islice(iterator, size - 1))
 
-hanzi_style = doc.styles.add_style('Hanzi Style', WD_STYLE_TYPE.PARAGRAPH)
-hanzi_style.font.name = 'DFKai-SB'
-hanzi_style_115 = doc.styles.add_style('Hanzi Style 115', WD_STYLE_TYPE.PARAGRAPH)
-hanzi_style_115.font.size = Pt(115)
-hanzi_style_115.base_style = doc.styles['Hanzi Style']
-hanzi_style_100 = doc.styles.add_style('Hanzi Style 100', WD_STYLE_TYPE.PARAGRAPH)
-hanzi_style_100.font.size = Pt(100)
-hanzi_style_100.base_style = doc.styles['Hanzi Style']
-hanzi_style_80 = doc.styles.add_style('Hanzi Style 80', WD_STYLE_TYPE.PARAGRAPH)
-hanzi_style_80.font.size = Pt(80)
-hanzi_style_80.base_style = doc.styles['Hanzi Style']
-hanzi_style_60 = doc.styles.add_style('Hanzi Style 60', WD_STYLE_TYPE.PARAGRAPH)
-hanzi_style_60.font.size = Pt(60)
-hanzi_style_60.base_style = doc.styles['Hanzi Style']
-hanzi_style_45 = doc.styles.add_style('Hanzi Style 45', WD_STYLE_TYPE.PARAGRAPH)
-hanzi_style_45.font.size = Pt(45)
-hanzi_style_45.base_style = doc.styles['Hanzi Style']
-pinyin_style = doc.styles.add_style("Pinyin", WD_STYLE_TYPE.PARAGRAPH)
-pinyin_style.base_style = doc.styles['Normal']
-pinyin_style.font.size = Pt(25)
 
-english_style = doc.styles.add_style("English", WD_STYLE_TYPE.CHARACTER)
-english_style.base_style = doc.styles['Normal']
-english_style.font.size = Pt(15)
+def process_page(doc, doc_table, terms, offset):
+    print(f"Processing word list of length={len(terms)}")
+    columns_num = 2
 
-example_style = doc.styles.add_style('Example', WD_STYLE_TYPE.PARAGRAPH)
-example_style.base_style = doc.styles['Hanzi Style']
-example_style.font.size = Pt(20)
-
-char_count_to_style = {1:'Hanzi Style 115',2:'Hanzi Style 100',3:'Hanzi Style 80',4:'Hanzi Style 60',5:'Hanzi Style 45'}
-
-lists_of_10_vocab_items = [vocab[i * 10:(i + 1) * 10] for i in range((len(vocab) + 9) // 10 )]
-lists_of_10_pinyin = [pinyin_list[i * 10:(i + 1) * 10] for i in range((len(pinyin_list) + 9) // 10 )]
-hanzi_10 = lists_of_10_vocab_items[0]
-pinyin_10 = lists_of_10_pinyin[0]
-
-print(doc.tables[0].rows[0].height_rule)
-
-def process_page(hanzi_10, pinyin_10, offset):
-    print("Processing word list of length="+ str(len(hanzi_10)))
-    for i in range(0,len(hanzi_10)):
-        (r,c) = (int((i+offset)/2), i % 2)
-        hanzi_p = doc_table.cell(r,c).paragraphs[0]
-        hanzi_p.text = hanzi_10[i][0]
-        hanzi_p.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        char_count = len(hanzi_10[i][0])
-        new_style = char_count_to_style[char_count]
+    for i, term in enumerate(terms, start=0):
+        # hanzi
+        (row, column) = (int((i + offset) / columns_num), i % columns_num)
+        hanzi_p = doc_table.cell(row, column).paragraphs[0]
+        hanzi_p.text = term["hanzi"]
+        hanzi_p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        char_count = len(term["hanzi"])
+        new_style = CHAR_COUNT_TO_STYLE[char_count]
         hanzi_p.style = new_style
-        (r,c) = (int((i+offset)/2) + 5, (i+1) % 2)
-        pinyin_cell = doc_table.cell(r,c)
-        pinyin_p = pinyin_cell.add_paragraph(pinyin_10[i][0],style=doc.styles['Pinyin'])
-        pinyin_p.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        if len(pinyin_10[i]) > 1:
-            eng_para = pinyin_p.add_run(pinyin_10[i][1],style=doc.styles['English'])
-            for p in pinyin_10[i][2:]:
-                para = pinyin_cell.add_paragraph(p,style=doc.styles['Example'])
-                para.alignment = WD_ALIGN_PARAGRAPH.CENTER
-            
 
-process_page(hanzi_10,pinyin_10,0)
+        # pinyin
+        (row, column) = (int((i + offset) / columns_num) + 5, (i + 1) % columns_num)
+        pinyin_cell = doc_table.cell(row, column)
+        pinyin_p = pinyin_cell.add_paragraph(
+            term["pinyin"] + " ", style=doc.styles["Pinyin"]
+        )
+        pinyin_p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
-offset = 0
-if len(lists_of_10_vocab_items) > 1:
-    for hanzi_10,pinyin_10 in zip(lists_of_10_vocab_items[1:],lists_of_10_pinyin[1:]):
-        for i in range(10):
-            new_row = doc_table.add_row()
-            new_row.height_rule = WD_ROW_HEIGHT_RULE.EXACTLY
-            new_row.height = Inches(2)
-            for new_cell in new_row.cells:
-                new_cell.vertical_alignment = WD_ALIGN_VERTICAL.CENTER
-        offset += 20
-        process_page(hanzi_10,pinyin_10,offset)
-    
-time_str = str(datetime.datetime.now().strftime("%Y%m%d_%H-%M-%S"))
-outputDocName = "flashcards-" + time_str + ".docx"
-doc.save(outputDocName)
+        if term["english"]:
+            pinyin_p.add_run(term["english"], style=doc.styles["English"])
+
+        if term["example"]:
+            para = pinyin_cell.add_paragraph(term["example"], style=doc.styles["Example"])
+            para.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+
+def get_doc():
+    doc = Document("FlashcardTemplate.docx")
+
+    hanzi_style = doc.styles.add_style("Hanzi Style", WD_STYLE_TYPE.PARAGRAPH)
+    hanzi_style.font.name = "DFKai-SB"
+    hanzi_style_115 = doc.styles.add_style("Hanzi Style 115", WD_STYLE_TYPE.PARAGRAPH)
+    hanzi_style_115.font.size = Pt(115)
+    hanzi_style_115.base_style = doc.styles["Hanzi Style"]
+    hanzi_style_100 = doc.styles.add_style("Hanzi Style 100", WD_STYLE_TYPE.PARAGRAPH)
+    hanzi_style_100.font.size = Pt(100)
+    hanzi_style_100.base_style = doc.styles["Hanzi Style"]
+    hanzi_style_80 = doc.styles.add_style("Hanzi Style 80", WD_STYLE_TYPE.PARAGRAPH)
+    hanzi_style_80.font.size = Pt(80)
+    hanzi_style_80.base_style = doc.styles["Hanzi Style"]
+    hanzi_style_60 = doc.styles.add_style("Hanzi Style 60", WD_STYLE_TYPE.PARAGRAPH)
+    hanzi_style_60.font.size = Pt(60)
+    hanzi_style_60.base_style = doc.styles["Hanzi Style"]
+    hanzi_style_45 = doc.styles.add_style("Hanzi Style 45", WD_STYLE_TYPE.PARAGRAPH)
+    hanzi_style_45.font.size = Pt(45)
+    hanzi_style_45.base_style = doc.styles["Hanzi Style"]
+    pinyin_style = doc.styles.add_style("Pinyin", WD_STYLE_TYPE.PARAGRAPH)
+    pinyin_style.base_style = doc.styles["Normal"]
+    pinyin_style.font.size = Pt(25)
+
+    english_style = doc.styles.add_style("English", WD_STYLE_TYPE.CHARACTER)
+    english_style.base_style = doc.styles["Normal"]
+    english_style.font.size = Pt(15)
+
+    example_style = doc.styles.add_style("Example", WD_STYLE_TYPE.PARAGRAPH)
+    example_style.base_style = doc.styles["Hanzi Style"]
+    example_style.font.size = Pt(20)
+
+    return doc
+
+
+def main():
+    pinyin_list = []
+    regex = re.compile(r"^(?P<hanzi>\S+)\s*(\((?P<english>.+)\))?\s*(?P<example>.+)?$")
+    doc = get_doc()
+    doc_table = doc.tables[0]
+
+    with codecs.open("flashcard-input.txt", "r", "utf-8-sig") as fp:
+        for line in fp.readlines():
+            match = regex.match(line.strip())
+            if match:
+                term_info = match.groupdict()
+                term_info["pinyin"] = pinyin.get(term_info["hanzi"])
+                pinyin_list.append(term_info)
+
+    rows_in_2_pages = 10
+
+    for page_index, chunk in enumerate(chunks(pinyin_list, size=rows_in_2_pages), start=0):
+        offset = page_index * rows_in_2_pages * 2
+
+        # extend table for additional pages
+        if offset:
+            for i in range(rows_in_2_pages):
+                new_row = doc_table.add_row()
+                new_row.height_rule = WD_ROW_HEIGHT_RULE.EXACTLY
+                new_row.height = Inches(2)
+                for new_cell in new_row.cells:
+                    new_cell.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER
+
+        process_page(doc, doc_table, list(chunk), offset)
+
+    time_str = str(datetime.datetime.now().strftime("%Y%m%d_%H-%M-%S"))
+    output_doc_name = "flashcards-" + time_str + ".docx"
+    doc.save(output_doc_name)
+
+
+if __name__ == "__main__":
+    main()
